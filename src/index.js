e368c56703e27ec7382e764c56f5fb831665e099
louddonut <roanpaulus@icloud.com>
2021-04-26 15:37:43 +0200

    added half finished tabbrowsing bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import { loadPage } from "../modules/load_page.js";
+import { tabbedBrowsing } from "../modules/tabbed_browsing.js";
 
 loadPage.home();
+tabbedBrowsing.addTabBar(2);
+
+const tabs = document.querySelectorAll(".individual-tabs");
+
+let toggle = false;
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", (e) => {
+
+        if (!toggle && e.target.id === "tab1") {
+            tabbedBrowsing.dropDown("menu");
+            toggle = true;
+        } else if (!toggle && e.target.id === "tab2") {
+            tabbedBrowsing.dropDown("contact");
+            toggle = true;
+        } else if (toggle && e.target.id === "tab1") {
+            tabbedBrowsing.dropDown("remove");
+            tabbedBrowsing.dropDown("menu");
+            toggle = false;
+        } else if (toggle && e.target.id === "tab2") {
+            tabbedBrowsing.dropDown("remove");
+            tabbedBrowsing.dropDown("contact");
+            toggle = false;
+        }
+
+    });
+});
